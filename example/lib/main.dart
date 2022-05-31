ebd474cf7665d11f35e35e7f5d6d3da12e9ceb93
= <gunhosong79@gmail.com>
2022-05-31 13:11:27 +0900

    v1

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:stacked_list/stacked_list.dart';
 
 void main() {
@@ -16,45 +13,25 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await StackedList.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
+    Map<String, List> data = {
+      'user1': ['1', '2', '3', '4'],
+      'user2': ['A', 'B', 'C']
+    };
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: StackedList(data: data),
         ),
       ),
     );
--- lib/stacked_list.dart
+++ lib/stacked_list.dart
@@ -1,13 +1,115 @@
+import 'package:flutter/material.dart';
 
-import 'dart:async';
+class StackedList extends StatefulWidget {
+  final double lineHeight;
+  final Map<String, List> data;
 
-import 'package:flutter/services.dart';
+  const StackedList({
+    Key? key,
+    required this.data,
+    this.lineHeight = 80,
+  }) : /*assert(titleBuilder != null),*/
+        super(key: key);
 
-class StackedList {
-  static const MethodChannel _channel = MethodChannel('stacked_list');
+  @override
+  State<StackedList> createState() => _StackedListState();
+}
 
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+class _StackedListState extends State<StackedList> {
+  late List<bool> isClickedList;
+  late List<String> titleList;
+  late List<List<dynamic>> contentList;
+
+  @override
+  void initState() {
+    titleList = widget.data.keys.toList();
+    contentList = widget.data.values.toList();
+    isClickedList = List.filled(titleList.length, false);
+    super.initState();
+  }
+
+  void setClickList(int index) {
+    setState(() {
+      isClickedList[index] = !isClickedList[index];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: List.generate(
+          titleList.length,
+          (titleIndex) => Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    GestureDetector(
+                      onTap: () => setClickList(titleIndex),
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: 70,
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: titleWidget(titleIndex),
+                        ),
+                      ),
+                    ),
+                    AnimatedContainer(
+                      duration: const Duration(milliseconds: 200),
+                      width: double.infinity,
+                      height: isClickedList[titleIndex]
+                          ? widget.lineHeight * contentList[titleIndex].length
+                          : widget.lineHeight,
+                      child: Stack(
+                        children: List.generate(
+                          contentList[titleIndex].length,
+                          (contentIndex) => AnimatedPositioned(
+                            duration: const Duration(milliseconds: 200),
+                            left: isClickedList[titleIndex]
+                                ? 0
+                                : contentIndex.toDouble() * 2,
+                            top: isClickedList[titleIndex]
+                                ? contentIndex.toDouble() * widget.lineHeight
+                                : 0,
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 3.0),
+                              child: myBox(
+                                  contentList[titleIndex][contentIndex],
+                                  widget.lineHeight),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )),
+    );
+  }
+
+  Widget titleWidget(int titleIndex) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Icon(Icons.ac_unit),
+        Text(
+          ' ${titleList[titleIndex]}',
+          style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+        ),
+        const Icon(Icons.ac_unit),
+      ],
+    );
+  }
+
+  Widget myBox(String s, double lineHeight) {
+    return Container(
+        width: MediaQuery.of(context).size.width * 0.8,
+        height: lineHeight - 6,
+        decoration: BoxDecoration(
+            border: Border.all(width: 1), color: const Color(0xffeff0f3)),
+        child: Text(s));
   }
 }
